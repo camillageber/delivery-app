c9c3049d6233129064bc7718d4205e0dd9684d5e
Murilo Costa <mcosta12358@gmail.com>
2022-11-30 00:16:29 -0300

    feat: criando a migration da tabela sales

---
+++ back-end/src/database/migrations/02-CreateSales.js
@@ -0,0 +1,58 @@
+'use strict';
+
+module.exports = {
+    up: async(queryInterface, Sequelize) => {
+        await queryInterface.createTable('users', {
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            userId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE',
+                references:{
+                  model: 'users',
+                  key: 'id',
+                }
+            },
+            sellerId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE',
+                references:{
+                  model: 'users',
+                  key: 'id',
+                }
+            },
+            totalPrice: {
+                type: Sequelize.DECIMAL(9,2),
+                allowNull: false,
+            },
+            deliveryAddress: {
+                type: Sequelize.STRING(100),
+                allowNull: false,
+            },
+            deliveryNumber: {
+                type: Sequelize.STRING(50),
+                allowNull: false,
+            },
+            saleDate: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            status: {
+                type: Sequelize.STRING(50),
+                allowNull: false,
+            }
+        });
+
+        down: async (queryInterface, Sequelize) => {
+            await queryInterface.dropTable('sales');
+        }
+    }
+}
