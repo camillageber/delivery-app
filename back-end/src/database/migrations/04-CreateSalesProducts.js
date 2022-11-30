1ce56c1989881b4e435ce68a1453a3cf32e88835
Murilo Costa <mcosta12358@gmail.com>
2022-11-30 00:25:22 -0300

    feat: criando a migration da tabela salesProducts

---
+++ back-end/src/database/migrations/04-CreateSalesProducts.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+     await queryInterface.createTable('salesProducts', { 
+      saleId: {
+         type: Sequelize.INTEGER,
+         references: {
+            model: 'sales',
+            key: 'id',
+         },
+         onUpdate: 'CASCADE',
+         onDelete: 'CASCADE',
+         primaryKey: true,
+      },
+      productId: {
+         type: Sequelize.INTEGER,
+         references: {
+            model: 'products',
+            key: 'id',
+         },
+         onUpdate: 'CASCADE',
+         onDelete: 'CASCADE',
+         primaryKey: true,
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      }
+     });
+    
+  },
+
+  down: async (queryInterface, Sequelize) => {
+   
+     await queryInterface.dropTable('salesProducts');
+   
+  }
+};
