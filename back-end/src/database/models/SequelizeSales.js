94474bffc5cd62b7113476037da420dfec76bfb9
Murilo Costa <mcosta12358@gmail.com>
2022-11-30 11:29:33 -0300

    fix: mundando para procedural os arquivos da model User e Sales

--- back-end/src/database/models/SequelizeSales.js
+++ back-end/src/database/models/SequelizeSales.js
@@ -1,57 +1,62 @@
-import { Model, DataTypes } from 'sequelize';
-import database from '.';
+const sequelize = require('sequelize');
 
-class Sales extends Model {}
+const Sales = (sequelize, DataTypes) => {
+  const Sales = sequelize.define("Sales", {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    userId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      references: {
+          model: 'users',
+          key: 'id'
+      }
+    },
+    sellerId: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      references: {
+          model: 'users',
+          key: 'id'
+      }
+    },
+    totalPrice: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    deliveryAddress: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    deliveryNumber: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    saleDate: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    status: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  }, {
+    modelName: 'sales',
+    timestamps: false,
+    sequelize: database,
+    tableName: 'sales',
+    underscored: true
+  });
 
-Sales.init({
-  id: {
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-    type: DataTypes.INTEGER,
-  },
-  userId: {
-    allowNull: false,
-    type: DataTypes.INTEGER,
-    references: {
-        model: 'users',
-        key: 'id'
-    }
-  },
-  sellerId: {
-    allowNull: false,
-    type: DataTypes.STRING,
-    references: {
-        model: 'users',
-        key: 'id'
-    }
-  },
-  totalPrice: {
-    allowNull: false,
-    type: DataTypes.STRING,
-  },
-  deliveryAddress: {
-    allowNull: false,
-    type: DataTypes.STRING,
-  },
-  deliveryNumber: {
-    allowNull: false,
-    type: DataTypes.STRING,
-  },
-  saleDate: {
-    allowNull: false,
-    type: DataTypes.STRING,
-  },
-  status: {
-    allowNull: false,
-    type: DataTypes.STRING,
-  },
-}, {
-  modelName: 'sales',
-  timestamps: false,
-  sequelize: database,
-  tableName: 'sales',
-  underscored: true
-});
+  Sales.associate = (models) => {
+    Sales.belongsTo(models.User, 
+      { foreignKey: 'userId', as: 'user' },
+      { foreignKey: 'sellerId', as: 'sellerId'})      
+  }
+}
 
 export default Sales;
--- back-end/src/database/models/SequelizeUser.js
+++ back-end/src/database/models/SequelizeUser.js
@@ -1,36 +1,39 @@
-import { Model, DataTypes } from 'sequelize';
-import database from '.';
+const sequelize = require('sequelize');
 
-class User extends Model {}
+const User = (sequelize, DataTypes) =>{
+  const User = sequelize.define("User",{
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    role: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  }, {
+    timestamps: false,
+    sequelize: database,
+    tableName: 'users',
+  });
 
-User.init({
-  id: {
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-    type: DataTypes.INTEGER,
-  },
-  name: {
-    allowNull: false,
-    type: DataTypes.STRING,
-  },
-  email: {
-    allowNull: false,
-    type: DataTypes.STRING,
-  },
-  password: {
-    allowNull: false,
-    type: DataTypes.STRING,
-  },
-  role: {
-    allowNull: false,
-    type: DataTypes.STRING,
-  },
-}, {
-  modelName: 'users',
-  timestamps: false,
-  sequelize: database,
-  tableName: 'users',
-});
+  User.associate = (models) => {
+    User.hasMany(models.Sales, { foreignKey: 'userId', as: 'userId'},
+     { foreignKey: 'sellerId', as: 'sellerId'})
+  }
+};
 
 export default User;
