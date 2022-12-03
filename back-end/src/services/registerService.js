287ba3d205d67d3e98cd5d2b61ddb281d4e84b7d
camillageber <camilla.geber@gmail.com>
2022-12-02 23:42:23 -0300

    refactor: changes on function register- hash md5 and generateToken modified

--- back-end/src/services/registerService.js
+++ back-end/src/services/registerService.js
@@ -1,6 +1,6 @@
+const md5 = require('md5');
 const db = require('../database/models');
 const CreateError = require('../utils/createError');
-const verifyHash = require('../utils/verifyPassword');
 const { generateToken } = require('../utils/tokenManager');
 
 const registerService = {
@@ -9,11 +9,11 @@ const registerService = {
         if (user) {
             throw new CreateError('ConflictError', 'Conflito: Usuário já cadastrado');
         }
-        const hashDB = user.password;
-        const senhaValidada = verifyHash(hashDB, password);
+        const senhaValidada = md5(password);
         const createdUser = await db.User
           .create({ name, email, password: senhaValidada, role: '' });
-        const token = await generateToken({ id: createdUser.id });
+          const { password: _, ...userWithoutPassword } = createdUser.toJSON();
+        const token = generateToken(userWithoutPassword);
         return { token };
     },
     
