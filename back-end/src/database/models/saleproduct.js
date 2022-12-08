a5a50696d4fa64218626794d01a258985727e047
camillageber <camilla.geber@gmail.com>
2022-12-07 21:51:46 -0300

    add: sale and saleproduct models

---
+++ back-end/src/database/models/saleproduct.js
@@ -0,0 +1,41 @@
+const SaleProduct = (sequelize, DataTypes) => {
+  const SaleProduct = sequelize.define('SaleProduct', {
+    saleId: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    productId: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    quantity: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    }
+  }, {
+    tableName: 'sales_products',
+    underscored: true,
+    timestamps: false,
+  });
+
+  SaleProduct.associate = (models) => {
+    models.Sales.belongsToMany(models.Product, {
+      as: 'products',
+      foreignKey: 'saleId',
+      otherKey: 'productId',
+      through: SaleProduct
+    });
+    models.Product.belongsToMany(models.Sales, {
+      as: 'sales',
+      foreignKey: 'productId',
+      otherKey: 'saleId',
+      through: SaleProduct
+    });
+  };
+
+  return SaleProduct;
+}
+
+module.exports = SaleProduct;
